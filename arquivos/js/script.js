6dc7744d2f86f854cb951b965dda9ddc1aedddc5
Kauê Matheus <kauematheus7@hotmail.com>
2023-12-05 00:03:53 -0300

    procurando a logica para o relogio

--- arquivos/js/script.js
+++ arquivos/js/script.js
@@ -1,4 +1,30 @@
+const hourHand = document.querySelector('.hours-hand')
+const minuteHand = document.querySelector('.minute-hand')
+const secondHand = document.querySelector('.second-hand')
+
+
+
 
 const getTime = () => {
-    const date = new Date();
-}
+    const date = new Date(); 
+
+    return {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+    };
+};
+
+
+setInterval( () => {
+    const { hours, minutes, seconds } = getTime();
+
+    const hourAngle = (360 / 12) * (hours + minutes / 60);
+    const minuteAngle = (360 / 60) * (minutes + seconds / 60);
+    const secondAngle = (360 / 60) * seconds;
+
+    hourHand.style.transform = `rotate(${hourAngle}deg)`;
+    minuteHand.style.transform = `rotate(${minuteAngle}deg)`;
+    secondHand.style.transform = `rotate(${secondAngle}deg)`;
+    
+}, 1000);
