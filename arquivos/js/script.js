7933aca4530c3b6e041d4a8b07abdb84012ecb46
Kauê Matheus <kauematheus7@hotmail.com>
2023-12-10 14:57:26 -0300

    video de fundo

--- arquivos/js/script.js
+++ arquivos/js/script.js
@@ -1,30 +1,31 @@
-const hourHand = document.querySelector('.hours-hand')
-const minuteHand = document.querySelector('.minute-hand')
-const secondHand = document.querySelector('.second-hand')
+
+document.addEventListener('DOMContentLoaded', function() {
+    const secondHand = document.querySelector('.second-hand')
+    const minuteHand = document.querySelector('.minute-hand')
+    const hourHand = document.querySelector('.hour-hand')
+
+    function updateClock() {
+        const now = new Date();
+
+        const seconds = now.getSeconds();
+        const minutes = now.getMinutes();
+        const hours = now.getHours();
+
+        const secondDegrees = (seconds / 60) * 360 + 90;
+        const minuteDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+        const hoursDegrees = ((hours % 12) / 12) * 360 + ((minutes / 60) + 30) + 90;
+  
+        secondHand.style.transform = `rotate(${secondDegrees}deg)`
+        minuteHand.style.transform =  `rotate(${minuteDegrees}deg)`
+        hourHand.style.transform =  `rotate(${hourDegrees}deg)`
+
+    }
+
+    setInterval(updateClock, 1000);
+    updateClock();
+
+});
 
 
 
 
-const getTime = () => {
-    const date = new Date(); 
-
-    return {
-        hours: date.getHours(),
-        minutes: date.getMinutes(),  
-        seconds: date.getSeconds(),
-    };
-};
-
-
-setInterval( () => {
-    const { hours, minutes, seconds } = getTime();
-
-    const hourAngle = (360 / 12) * (hours + minutes / 60);
-    const minuteAngle = (360 / 60) * (minutes + seconds / 60);
-    const secondAngle = (360 / 60) * seconds;
-
-    hourHand.style.transform = `rotate(${hourAngle}deg)`;
-    minuteHand.style.transform = `rotate(${minuteAngle}deg)`;
-    secondHand.style.transform = `rotate(${secondAngle}deg)`;
-    
-}, 1000);
