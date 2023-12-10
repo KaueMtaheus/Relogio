1697c9c4ff2301b528295997afe2cbd0514d634f
Kauê Matheus <kauematheus7@hotmail.com>
2023-12-10 16:05:59 -0300

    logica do relogio

--- arquivos/js/script.js
+++ arquivos/js/script.js
@@ -1,31 +1,26 @@
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
 
-document.addEventListener('DOMContentLoaded', function() {
-    const secondHand = document.querySelector('.second-hand')
-    const minuteHand = document.querySelector('.minute-hand')
-    const hourHand = document.querySelector('.hour-hand')
+const getTime = () => {
+    const date = new Date();
 
-    function updateClock() {
-        const now = new Date();
-
-        const seconds = now.getSeconds();
-        const minutes = now.getMinutes();
-        const hours = now.getHours();
-
-        const secondDegrees = (seconds / 60) * 360 + 90;
-        const minuteDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
-        const hoursDegrees = ((hours % 12) / 12) * 360 + ((minutes / 60) + 30) + 90;
-  
-        secondHand.style.transform = `rotate(${secondDegrees}deg)`
-        minuteHand.style.transform =  `rotate(${minuteDegrees}deg)`
-        hourHand.style.transform =  `rotate(${hourDegrees}deg)`
-
-    }
-
-    setInterval(updateClock, 1000);
-    updateClock();
-
-});
+    return {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+    };
+}
 
 
+setInterval(() => {
 
+    const { seconds, minutes, hours } = getTime();
 
+    secondHand.style.transform = `translate(0, -50%) rotate(${seconds * 6}deg)`
+    minuteHand.style.transform = `translate(0, -50%) rotate(${minutes * 6}deg)`
+    hoursHand.style.transform = `translate(0, -50%) rotate(${hours * 30}deg)`
+
+    
+
+}, 1000);
